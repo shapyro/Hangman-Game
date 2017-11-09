47cfc5e45be252b1fd96ea5c81912c3d8464e85f
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-11-08 20:50:24 -0600

    cleaning the slate

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,36 +7,68 @@
 //  once guess count is diminished display loss,
 //  otherwise display WIN once array has been met with guesses
 
-var bandArray = ["WEEN","Budos","Prince","Doors"];
-var wins = 0;
-var guessCount = 20;
-var startValue = 0;
-
+var bandArray = ["WEEN","BUDOS","PRINCE","DOORS"];
 
 var getBand = bandArray[Math.floor(Math.random() * bandArray.length)];
-console.log(getBand);
+  console.log(getBand);
 
-var bandString = getBand.split("");
-// var blanks = bandName.map(a=>'_');
-var blanks = bandString.map(function(a) {
-  return '_';
-})
+// var game = {
+//   guessed: [],
+//   guessCount: 20,
 
-document.onkeyup = function() {
-  var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-  console.log(userGuess); 
+// var game = {
+//   guessed: [],
+//   guessCount: 20,
+//   wins: 0,
+//   startValue: 0
+// }
 
-  while (getBand.indexOf(userGuess, startValue) !== -1) {
-    startValue = getBand.indexOf(userGuess, startValue) + 1;
-    if (userGuess.indexOf(bandString)) {
-      console.log("it's a letter!");
+//function(stringBand) {
+  var bandString = getBand.split("");
+  var blanks = bandString.map(a=>'_');
+  var blanksArray = [];
+  var guessCount = 20;
+  var guessed = [];
+
+  var blanks_span = "<span>" + blanks + "<span>";
+
+  document.querySelector('#blanks').innerHTML = blanks_span;
+  //document.querySelector('#blanks').innerHTML = bandString.map(a=>'_');
+
+  document.onkeyup = function(event) {
+    var userGuess = event.key.toUpperCase(); //String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userGuess); 
+    var startValue = 0;
+
+    // // update letters and fill blanks
+    // while (getBand.indexOf(userGuess, startValue) !== -1) {
+    // //blanks[bandString.indexOf(userGuess, startValue)].push(guessed);
+    //   startValue = getBand.indexOf(userGuess, startValue) + 1;
+    // //   //document.querySelector('#letters').innerHTML = guessed.push(" " + userGuess);
+    // //   //document.querySelector('#used').innerHTML = guessed.push(String.fromCharCode(userGuess));
+    // guessCount -1;
+    // console.log(guessCount)
+
+
+    //replace dash with letter
+    // for (i=0; i<bandString.length; i++) {
+    //   console.log(guessCount);
+      
+
+      if (bandString.indexOf(userGuess) > -1) {
+        console.log("it's a letter!");
+        document.querySelector('#used').innerHTML = guessed.push(String.fromCharCode(userGuess));
+
+        //console.log(userGuess);
+      } 
+      else {
+        console.log("Not a letter");
+        guessed.push(userGuess);
+        //console.log(userGuess);
+      }
     }
-    else {
-      console.log("Not a letter");
-    }
-  }
-  }
- 
+
+
 
 
 
