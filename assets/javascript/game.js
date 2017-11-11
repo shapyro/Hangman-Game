3b2aa47438670f39aeffe090bbc93b4a37ac1bdf
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2017-11-10 21:44:31 -0600

    unable to get wins or alert that letter has already been used

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,40 @@
-// Computer needs to choose band name from Array of bands
-// user needs to guess letter of band name
-// capture input of guesses from user
-// While guesses are less than 20
-//  display guesses in order of bandname Array
-//  update decreasing guess count 
-//  once guess count is diminished display loss,
-//  otherwise display WIN once array has been met with guesses
+//The Band Array
+//computer picks a band, strings a band, and makes blanks of bandwidth
+//variables for counting down guesses and pushing guess chars
 
 var bandArray = ["WEEN","BUDOS","PRINCE","DOORS"];
-
 var getBand = bandArray[Math.floor(Math.random() * bandArray.length)];
-  console.log(getBand);
-
 var bandString = getBand.split("");
 var blanks = bandString.map(a=>'_');
 var guessCount = 20;
 var guessed = [];
 
-
+//player1, pick a letter
+//letter to upper case to match array
+//letter is indeed a letter because regex
 document.onkeyup = function(event) {
   var userGuess = event.key.toUpperCase();
-
-  console.log(userGuess); 
   
   if(/^[a-zA-Z]+$/.test(userGuess)){
-  
-
-  var index = bandString.indexOf(userGuess) 
-  while (index >-1) {
-    console.log("it's a letter!");    
+    
+    //index used to update blanks with letters
+    var index = bandString.indexOf(userGuess);
+    
+    while (index >-1) {  
       blanks[index]=userGuess; 
       index = bandString.indexOf(userGuess, index+1); 
-  } 
+      document.querySelector('#blanks').innerHTML = blanks.join(" ");
+    } 
 
-  // else {
-  //   console.log("Not a letter");
-  // }
+    guessed.push(userGuess);
+    document.querySelector('#used').innerHTML = guessed.join(String.fromCharCode(userGuess));
 
-  document.querySelector('#blanks').innerHTML = blanks.join(" ");
-
-  guessed.push(userGuess);
-  console.log(guessed);
-  document.querySelector('#used').innerHTML = guessed.join(String.fromCharCode(userGuess));
-
-
-  guessCount--;
-  document.querySelector("#guessCount").innerHTML = guessCount;
-  if (guessCount == 0) {
-    var loser = "you lost"
-    document.querySelector('body').innerHTML = loser;
+    guessCount--;
+    document.querySelector("#guessCount").innerHTML = guessCount;
+    if (guessCount == 0) {
+      var loser = "you lost"
+      document.querySelector('body').innerHTML = loser;
+    } 
   }
 }
-}
+
